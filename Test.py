83928bd2433052c075516c7d265921a71cd98536
Radicrow <igoracatauassu03@gmail.com>
2024-08-23 12:25:52 -0300

    :fox: Adding test file :fox:

---
+++ Test.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+data = {'Name': ['Firestar', 'Brambleclaw', 'Jayfeather', 'Ivypool', 'Dovwing', 'Hollyleaf'],
+        'Class': ['Fighter', 'Fighter', 'Medicine cat', 'Fighter', 'Specialist', 'Specialist']}
+
+df = pd.DataFrame(data)
+
+
+
+
+specialist_pd = df[df['Class'] == 'Specialist']
+
+plt.figure(figsize=(10, 8))
+sns.countplot(x='Class', data=df, color = '#DC143C', alpha = 0.5)
+plt.title('Number of Members in Each Class')
+plt.xlabel('Class')
+plt.ylabel('Number of Members')
+plt.xticks(rotation=35)
+plt.show()
+
+print(specialist_pd)
