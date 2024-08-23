84ba7d2940e0fcba35f1ae8eb2fc6ed39190bb11
Radicrow <igoracatauassu03@gmail.com>
2024-08-23 12:27:15 -0300

    :cat: Adding test file :cat:

--- Test.py
+++ Test.py
@@ -12,7 +12,7 @@ df = pd.DataFrame(data)
 
 specialist_pd = df[df['Class'] == 'Specialist']
 
-plt.figure(figsize=(10, 8))
+plt.figure(figsize=(10, 9))
 sns.countplot(x='Class', data=df, color = '#DC143C', alpha = 0.5)
 plt.title('Number of Members in Each Class')
 plt.xlabel('Class')
